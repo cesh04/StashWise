57ecb667b536263706eec6da856550866b8c0410
Anshul Angolkar <anshulangolkar@Anshuls-MacBook-Air.local>
2024-11-25 01:55:33 +0530

    Updated code to work on

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:stashwise/pages/categories.dart';
-import 'package:stashwise/pages/dues.dart';
+import 'package:stashwise/pages/new_category.dart';
 import 'package:stashwise/pages/new_transaction.dart';
 import 'package:stashwise/pages/transaction_history.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:stashwise/pages/dues.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -16,20 +17,34 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   String name = 'User';
   String greeting = 'Hello';
-<<<<<<< HEAD
 
   double cashBalance = 0.0;
   double bankBalance = 0.0;
 
   final TextEditingController _categoryController = TextEditingController();
-=======
->>>>>>> e574842c2cc15396094b54d3d04185729c0bdd4a
 
   @override
   void initState() {
     super.initState();
+    _fetchUserDetails();
     _updateGreeting();
     _fetchBalances();
+  }
+
+  Future<void> _fetchUserDetails() async {
+    final database = openDatabase(
+      join(await getDatabasesPath(), 'fund_management.db'),
+    );
+
+    final db = await database;
+    final List<Map<String, dynamic>> personalDetails =
+        await db.query('personal_details');
+
+    if (personalDetails.isNotEmpty) {
+      setState(() {
+        name = personalDetails[0]['name'];
+      });
+    }
   }
 
   void _updateGreeting() {
@@ -38,7 +53,7 @@ class _HomeState extends State<Home> {
     setState(() {
       if (hour < 12) {
         greeting = 'Good Morning';
-      } else if (hour < 16) {
+      } else if (hour < 4) {
         greeting = 'Good Afternoon';
       } else {
         greeting = 'Good Evening';
@@ -46,8 +61,7 @@ class _HomeState extends State<Home> {
     });
   }
 
-<<<<<<< HEAD
-  void _showAddCategoryDialog(BuildContext context) {
+  /*void _showAddCategoryDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -96,7 +110,7 @@ class _HomeState extends State<Home> {
       {'category_name': category},
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
-  }
+  } */
 
   Future<void> _fetchBalances() async {
     final db = await openDatabase(
@@ -124,8 +138,6 @@ class _HomeState extends State<Home> {
     });
   }
 
-=======
->>>>>>> e574842c2cc15396094b54d3d04185729c0bdd4a
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -133,7 +145,7 @@ class _HomeState extends State<Home> {
         color: Colors.white,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Container(
               margin: const EdgeInsets.all(16.0),
@@ -153,29 +165,16 @@ class _HomeState extends State<Home> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                // Cash Box
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => TransactionHistoryPage(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(10.0),
-                      padding: const EdgeInsets.symmetric(vertical: 42),
-                      decoration: BoxDecoration(
-                        gradient: const LinearGradient(
-                          colors: [Color(0xFF1F62FF), Colors.cyan],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
-                        borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.symmetric(vertical: 42),
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [Color(0xFF1F62FF), Colors.cyan],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
                       ),
-<<<<<<< HEAD
                       borderRadius: BorderRadius.circular(15),
                     ),
                     child: Padding(
@@ -193,64 +192,33 @@ class _HomeState extends State<Home> {
                                 color: Color(0xFFE6E8E6),
                                 fontWeight: FontWeight.w800,
                                 fontSize: 32.0, // Larger amount font size
-=======
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 12.0, bottom: 12.0),
-                        child: Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text(
-                                '\$350',
-                                style: TextStyle(
-                                  fontFamily: 'Open Sans',
-                                  color: Color(0xFFE6E8E6),
-                                  fontWeight: FontWeight.w800,
-                                  fontSize: 32.0, // Larger amount font size
-                                ),
->>>>>>> e574842c2cc15396094b54d3d04185729c0bdd4a
                               ),
-                              Text(
-                                'Cash',
-                                style: TextStyle(
-                                  fontFamily: 'Open Sans',
-                                  color: Color(0xFFE6E8E6),
-                                  fontSize: 20.0, // Smaller label font size
-                                  fontWeight: FontWeight.w600,
-                                ),
+                            ),
+                            Text(
+                              'Cash',
+                              style: TextStyle(
+                                fontFamily: 'Open Sans',
+                                color: Color(0xFFE6E8E6),
+                                fontSize: 20.0, // Smaller label font size
+                                fontWeight: FontWeight.w600,
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
                   ),
                 ),
-                // Bank Box
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => TransactionHistoryPage(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(8.0),
-                      padding: const EdgeInsets.symmetric(vertical: 42),
-                      decoration: BoxDecoration(
-                        gradient: const LinearGradient(
-                          colors: [Colors.cyan, Color(0xFF1F62FF)],
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                        ),
-                        borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.symmetric(vertical: 42),
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [Colors.cyan, Color(0xFF1F62FF)],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
                       ),
-<<<<<<< HEAD
                       borderRadius: BorderRadius.circular(15),
                     ),
                     child: Padding(
@@ -268,36 +236,18 @@ class _HomeState extends State<Home> {
                                 color: Color(0xFFE6E8E6),
                                 fontWeight: FontWeight.w800,
                                 fontSize: 32.0, // Larger amount font size
-=======
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 12.0, bottom: 12.0),
-                        child: Align(
-                          alignment: Alignment.bottomLeft,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: const [
-                              Text(
-                                '\$7500',
-                                style: TextStyle(
-                                  fontFamily: 'Open Sans',
-                                  color: Color(0xFFE6E8E6),
-                                  fontWeight: FontWeight.w800,
-                                  fontSize: 32.0, // Larger amount font size
-                                ),
->>>>>>> e574842c2cc15396094b54d3d04185729c0bdd4a
                               ),
-                              Text(
-                                'Bank',
-                                style: TextStyle(
-                                  fontFamily: 'Open Sans',
-                                  color: Color(0xFFE6E8E6),
-                                  fontSize: 20.0, // Smaller label font size
-                                  fontWeight: FontWeight.w600,
-                                ),
+                            ),
+                            Text(
+                              'Bank',
+                              style: TextStyle(
+                                fontFamily: 'Open Sans',
+                                color: Color(0xFFE6E8E6),
+                                fontSize: 20.0, // Smaller label font size
+                                fontWeight: FontWeight.w600,
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
                       ),
                     ),
@@ -323,10 +273,9 @@ class _HomeState extends State<Home> {
                         color: Color(0xFF1F62FF)),
                     onTap: () {
                       Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => TransactionHistoryPage()),
-                      );
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TransactionHistoryPage()));
                     },
                   ),
                   const SizedBox(height: 8),
@@ -343,10 +292,8 @@ class _HomeState extends State<Home> {
                     trailing: const Icon(Icons.arrow_forward_ios,
                         color: Color(0xFF1F62FF)),
                     onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => DuesPage()),
-                      );
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => DuesPage()));
                     },
                   ),
                   const SizedBox(height: 8),
@@ -364,9 +311,9 @@ class _HomeState extends State<Home> {
                         color: Color(0xFF1F62FF)),
                     onTap: () {
                       Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => CategoriesPage()),
-                      );
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => CategoriesPage()));
                     },
                   ),
                 ],
@@ -375,8 +322,8 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
+      // Adding the Floating Action Button
       floatingActionButton: FloatingActionButton(
-<<<<<<< HEAD
         onPressed: () async {
           // Navigate to NewTransactionPage and wait for a result
           final result = await Navigator.push(
@@ -391,21 +338,14 @@ class _HomeState extends State<Home> {
         },
 
         backgroundColor: const Color(0xFF1F62FF), // Matching theme color
-=======
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NewTransactionPage()));
-        },
-        backgroundColor: const Color(0xFF1F62FF),
->>>>>>> e574842c2cc15396094b54d3d04185729c0bdd4a
         child: const Icon(
           Icons.add,
-          size: 36.0,
-          color: Colors.white,
+          size: 36.0, // Slightly larger icon size for prominence
+          color: Colors.white, // White color for contrast
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.endFloat, // Center FAB at bottom
     );
   }
 }
